5a828c2af14775bb24e7a215cfeff95ea68c0ecf
Mike-96 <Kevingarciamorales61@gmail.com>
2023-03-01 17:03:27 -0600

    01/03/23

---
+++ PHP/API PUBG/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Instanciamos los archivos de PyScript desde su mismo Servidor -->
+<link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
+
+    <title>API </title>
+</head>
+<body>
+    <main>
+        <header>
+            <div>
+                <a href=""></a>
+
+            </div>
+        </header>
+
+    </main>
+    <script src="https://gist.github.com/xombra/11336106.js"></script>
+
+    <!-- Instanciamos los archivos de PyScript desde su mismo Servidor -->
+    <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
+    <py-script src="api.py"></py-script>
+    <script src="main.js"></script>
+</body>
+</html>
